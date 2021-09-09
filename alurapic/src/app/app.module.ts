4f94244f125a9bcdf7da613f7bbe1368980a1e5a
larissa.mauricio@magrathealabs.com <larissa.mauricio@magrathealabs.com>
2021-09-08 21:51:36 -0300

    added a api path

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,9 +11,10 @@ import { PhotosModule } from './photos/photos.module';
     AppComponent
     ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
