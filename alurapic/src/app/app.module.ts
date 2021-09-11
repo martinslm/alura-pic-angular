96ba83e06542f75edd6de914a61c6cb243b88878
larissa.mauricio@magrathealabs.com <larissa.mauricio@magrathealabs.com>
2021-09-10 23:09:57 -0300

    creating a component by command line and learning about commonModule

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotosModule } from './photos/photos.module';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    PhotoListComponent
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
