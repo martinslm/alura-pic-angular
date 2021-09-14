a5a26f52493f69ffdfb1003ad26c487c9b3aaf7c
larissa.mauricio@magrathealabs.com <larissa.mauricio@magrathealabs.com>
2021-09-14 16:50:08 -0300

    feat: created a not found page

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    PhotosModule
+    PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
