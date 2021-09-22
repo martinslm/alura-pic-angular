dd1e0bbd6c1e32843931506eca2da7d887615e8e
larissa.mauricio@magrathealabs.com <larissa.mauricio@magrathealabs.com>
2021-09-22 18:12:16 -0300

    created first pipe

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -6,13 +6,15 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { CommonModule } from '@angular/common';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
     declarations: [
         PhotoComponent, 
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescription
     ],
     imports: [HttpClientModule, CommonModule]
 })
