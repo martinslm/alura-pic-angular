fbd3b57a74fef935f4ea8f5a22809e178b5a4533
larissa.mauricio@magrathealabs.com <larissa.mauricio@magrathealabs.com>
2021-09-23 14:52:53 -0300

    implemented first subject

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>()
 
   constructor(private activatedRoute: ActivatedRoute){
   }
   
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe()
+  }
+  
   ngOnInit(): void{
     this.photos = this.activatedRoute.snapshot.data['photos']
+
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter)
   }
 
   onKeyUp(target : any) {
     if(target instanceof EventTarget) {
       var element = target as HTMLInputElement;
-      this.filter = element.value;
+      this.debounce.next(element.value);
     }
   }
 
 }
+
