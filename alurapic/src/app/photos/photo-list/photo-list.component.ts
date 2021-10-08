1f5d85b0f96d66eeeb70d079e8a4dc628dac45b7
larissa.mauricio@magrathealabs.com <larissa.mauricio@magrathealabs.com>
2021-10-08 13:30:01 -0300

    implemented the search component and the first directive

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -10,10 +8,9 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   username: string = '';
@@ -21,30 +18,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     private photoService: PhotoService){
   }
 
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe()
-  }
   
   ngOnInit(): void{
     this.username = this.activatedRoute.snapshot.params.username;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter)
-  }
-
-  onKeyUp(target : any) {
-    if(target instanceof EventTarget) {
-      var element = target as HTMLInputElement;
-      this.debounce.next(element.value);
-    }
   }
 
   load(): void {
       this.photoService
       .listFromUserPaginated(this.username, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = ''
         this.photos = this.photos.concat(photos)
         if(!photos.length)this.hasMore = false
       });
